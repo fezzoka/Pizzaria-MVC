ad89333de2de38f965986ddba848bced0acd98c0
FELIPE TADEU MARICOTA <felipe.tmaricota@senacsp.edu.br>
2022-08-18 22:04:49 -0300

    front end 18.8

--- Pizzaria_MVC/wwwroot/js/site.js
+++ Pizzaria_MVC/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//navbar
+$(document).ready(function () {
+    $('#navbarDropdown').mouseenter(function () {
+        $('.dropdown-menu').slideToggle(300, "linear");
+    });
+
+    $('.dropdown-menu').mouseleave(function () {
+        $(this).slideToggle(300, "linear");
+    });
+});
+//navbar
