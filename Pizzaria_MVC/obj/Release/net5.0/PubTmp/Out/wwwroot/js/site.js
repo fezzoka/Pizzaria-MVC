613de202974f5f93718f46bc4f1226d765df53de
FELIPE TADEU MARICOTA <felipe.tmaricota@senacsp.edu.br>
2022-08-23 19:37:44 -0300

    Update Fundo escuro

---
+++ Pizzaria_MVC/obj/Release/net5.0/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+//navbar
+$(document).ready(function () {
+    $('#navbarDropdown').mouseenter(function () {
+        $('.dropdown-menu').slideToggle(300, "linear");
+    });
+
+    $('.dropdown-menu').mouseleave(function () {
+        $(this).slideToggle(300, "linear");
+    });
+});
+//navbar
